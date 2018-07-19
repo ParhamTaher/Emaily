c7e87865503bb5a7b0b1de6c01a625aa9d526202
ParhamTaher <parham.taher@gmail.com>
2018-07-19 16:29:49 -0400

    Changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+	res.send({ hello: 'there' });
 });
 
 // process env port is the port the host will assign
