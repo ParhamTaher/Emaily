e16435ea3e59a9aaf531f0eb2e7c82bf3d62d687
ParhamTaher <parham.taher@gmail.com>
2018-08-23 23:47:47 -0400

    Refactored code

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
+// Can remove curly braces and return keyword if one term inside
+// Refactored to Async Await
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get('/api/current_user');
 
-    axios.get('/api/current_user');
-
-}
+    dispatch({ type: FETCH_USER, payload: res.data });     
+};
